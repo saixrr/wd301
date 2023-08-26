ed32f67e8a65b11f8395dd157c4a09065fda5359
saixrr <111263091+saixrr@users.noreply.github.com>
2023-08-26 15:02:21 +0530

    commit changes for level4

--- smarter-tasks/src/TaskApp.tsx
+++ smarter-tasks/src/TaskApp.tsx
@@ -1,31 +1,38 @@
-import React from "react";
+
+import React, { useEffect } from "react";
 import { TaskItem } from "./types";
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
 import "./TaskCard.css";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+React.useState<TaskAppState>;
 
-interface TaskAppProp {}
+// interface TaskAppProp {}
 interface TaskAppState {
   tasks: TaskItem[];
 }
-class TaskApp extends React.Component<TaskAppProp, TaskAppState> {
-  constructor(props: TaskAppProp) {
-    super(props);
-    this.state = {
-      tasks: [],
+const TaskApp = () => {
+  const [TaskAppState, setTaskAppState] = useLocalStorage<TaskAppState>("tasks", {
+    tasks: [],
+  });
+  React.useEffect(() => {
+    const id = setTimeout(() => {
+      console.log(`Saved ${TaskAppState.tasks.length} items to backend...`);
+    }, 5000);
+    return () => {
+      console.log("clear or cancel any existing network call");
+      clearTimeout(id);
     };
-  }
-  addTask = (task: TaskItem) => {
-    this.setState((state) => {
-      return {
-        tasks: [...state.tasks, task],
-      };
-    });
+  },[TaskAppState.tasks]);
+  const addTask = (task: TaskItem) => {
+    setTaskAppState({ tasks: [...TaskAppState.tasks,task]});
   };
-
-  render() {
-    return (
-      <div className="container py-10 max-w-4xl mx-auto">
+  const deleteTask = (indexToDelete: number) => {
+    const updatedTasks = TaskAppState.tasks.filter((_, index) => index !== indexToDelete);
+    setTaskAppState({ tasks: updatedTasks });
+  };
+  return (
+    <div className="container py-10 max-w-4xl mx-auto">
         <h1 className="text-3xl mb-2 font-bold text-slate-700">
           Smarter Tasks
         </h1>
@@ -38,12 +45,13 @@ class TaskApp extends React.Component<TaskAppProp, TaskAppState> {
             <h1 className="text-slate-500 text-xl font-bold text-center mb-2">
               Pending
             </h1>
-            <TaskForm addTask={this.addTask}/>
-            <TaskList tasks={this.state.tasks} />
+            <TaskForm addTask={addTask}/>
+            <ul>
+            <TaskList tasks={TaskAppState.tasks} deleteTask={deleteTask}/>
+            </ul>
           </div>
         </div>
       </div>
-    );
-  }}
-
+  )
+}
 export default TaskApp;
