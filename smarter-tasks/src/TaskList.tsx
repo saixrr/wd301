50816c7b24aa66ab917dafb2fa0b49ba4b0d7c1e
saixrr <111263091+saixrr@users.noreply.github.com>
2023-08-26 15:03:34 +0530

    commit changes for level4

--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import Task from "./Task";
 import { TaskItem } from "./types";
+
 interface Props {
   tasks: TaskItem[];
+  deleteTask: (indexToDelete: number) => void;
 }
-interface State {}
-class TaskList extends React.Component<Props, State> {
-  
-  render() {
-    return this.props.tasks.map((task, idx) => (
-      <Task key={idx} todoTitle={task.todoTitle} todoDueDate={task.todoDueDate} todoDescription={task.todoDescription}/>
-    ));
-  }
-}
+
+const TaskList = (props: Props) => {
+  return props.tasks.map((task, idx) => (
+    <li key={idx} className="TaskItem shadow-md border border-slate-100">
+      <Task
+        todoTitle={task.todoTitle}
+        todoDueDate={task.todoDueDate}
+        todoDescription={task.todoDescription}
+      />
+      <button
+        className="deleteTaskButton"
+        onClick={() => props.deleteTask(idx)}
+      >
+        Delete
+      </button>
+    </li>
+  ));
+};
+
 export default TaskList;
