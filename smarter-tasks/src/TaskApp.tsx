4f3c4358ee6ed0a6cd026725b9ff1f019084c465
saixrr <111263091+saixrr@users.noreply.github.com>
2023-08-26 16:21:58 +0530

    Update TaskApp.tsx

--- smarter-tasks/src/TaskApp.tsx
+++ smarter-tasks/src/TaskApp.tsx
@@ -1,56 +1,71 @@
-
-import  { useEffect } from "react";
-import { TaskItem } from "./types";
+import { useEffect } from "react";
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
 import "./TaskCard.css";
 import { useLocalStorage } from "./hooks/useLocalStorage";
+import { TaskItem } from "./types";
 
-// interface TaskAppProp {}
 interface TaskAppState {
   tasks: TaskItem[];
 }
+
 const TaskApp = () => {
-  const [TaskAppState, setTaskAppState] = useLocalStorage<TaskAppState>("tasks", {
-    tasks: [],
-  });
+  const [taskAppState, setTaskAppState] = useLocalStorage<TaskAppState>(
+    "tasks",
+    {
+      tasks: [],
+    }
+  );
+
   useEffect(() => {
     const id = setTimeout(() => {
-      console.log(`Saved ${TaskAppState.tasks.length} items to backend...`);
+      console.log(`Saved ${taskAppState.tasks.length} items to backend...`);
     }, 5000);
+
     return () => {
       console.log("clear or cancel any existing network call");
       clearTimeout(id);
     };
-  },[TaskAppState.tasks]);
+  }, [taskAppState.tasks]);
+
   const addTask = (task: TaskItem) => {
-    setTaskAppState({ tasks: [...TaskAppState.tasks,task]});
+    if (!task.todoTitle || !task.todoDueDate) {
+      return;
+    }
+
+    setTaskAppState({ tasks: [...taskAppState.tasks, task] });
   };
+
   const deleteTask = (indexToDelete: number) => {
-    const updatedTasks = TaskAppState.tasks.filter((_, index) => index !== indexToDelete);
+    const updatedTasks = taskAppState.tasks.filter(
+      (_, idx) => idx !== indexToDelete
+    );
     setTaskAppState({ tasks: updatedTasks });
   };
+
   return (
     <div className="container py-10 max-w-4xl mx-auto">
-        <h1 className="text-3xl mb-2 font-bold text-slate-700">
-          Smarter Tasks
-        </h1>
-        <h1 className="text-lg mb-6 text-slate-600">
-          <span className="font-bold">Project: </span>
-          Graduation Final Year Project (Revamp college website)
-        </h1>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="border border-slate-200 rounded-xl p-4">
-            <h1 className="text-slate-500 text-xl font-bold text-center mb-2">
-              Pending
-            </h1>
-            <TaskForm addTask={addTask}/>
-            <ul>
-            <TaskList tasks={TaskAppState.tasks} deleteTask={deleteTask}/>
-            </ul>
-          </div>
+      <h1 className="text-3xl mb-2 font-bold text-slate-700">Smarter Tasks</h1>
+      <h1 className="text-lg mb-6 text-slate-600">
+        <span className="font-bold">Project: </span>
+        Graduation Final Year Project (Revamp college website)
+      </h1>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="border border-slate-200 rounded-xl p-4">
+          <h1 className="text-slate-500 text-xl font-bold text-center mb-2">
+            Pending
+          </h1>
+          <TaskForm addTask={addTask} />
+          <ul>
+            <TaskList
+              tasks={taskAppState.tasks}
+              deleteTask={deleteTask}
+            />
+          </ul>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
+
 export default TaskApp;
