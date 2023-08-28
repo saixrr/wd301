7d2b156ffb58e86c3f216fd5372e91bfad5bbfc9
saixrr <111263091+saixrr@users.noreply.github.com>
2023-08-28 23:06:23 +0530

    Update Task.tsx

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,13 +1,14 @@
 // import React from 'react';
 import "./TaskCard.css";
-import { TaskItem } from './types';
-// interface TaskProp {
-//   todoTitle: string;
-//   todoDescription:string;
-//   todoDueDate:string;
-// }
+// import { TaskItem } from './types';
+interface TaskProp {
+  todoTitle: string;
+  todoDescription:string;
+  todoDueDate:string;
+  onDelete:()=>void;
+}
 
-const Task=(props:TaskItem)=>{
+const Task=(props:TaskProp)=>{
   return (
       <div className="TaskItem shadow-md border border-slate-100">
         <h2 className="text-base font-bold my-1">Title:{props.todoTitle}</h2>
@@ -17,6 +18,11 @@ const Task=(props:TaskItem)=>{
         <p className="text-sm text-slate-500">
           Description:{props.todoDescription} 
         </p>
+        <button
+          className="deleteTaskButton"
+            onClick={() => props.onDelete()}>
+            Delete
+        </button>
       </div>
     );
 };
