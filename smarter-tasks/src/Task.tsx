d9ed63f34283aef65b6fbf57a0b2e35e7124a2bf
saixrr <111263091+saixrr@users.noreply.github.com>
2023-08-29 11:44:33 +0530

    Update Task.tsx

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -12,10 +12,10 @@ const Task=(props:TaskProp)=>{
   return (
       <div className="TaskItem shadow-md border border-slate-100">
         <h2 className="text-base font-bold my-1">Title:{props.todoTitle}</h2>
-        <p className="text-sm text-slate-500">
+        <p className="TaskItem text-sm text-slate-500">
           Due Date:{props.todoDueDate}
         </p>
-        <p className="text-sm text-slate-500">
+        <p className="TaskItem text-sm text-slate-500">
           Description:{props.todoDescription} 
         </p>
         <button
