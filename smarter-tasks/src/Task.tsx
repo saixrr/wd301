c7571aeebc51616c0f583c9a494c22b71d2d0ec5
saixrr <111263091+saixrr@users.noreply.github.com>
2023-08-29 13:15:39 +0530

    Update Task.tsx

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,32 +1,24 @@
-// import React from 'react';
 import "./TaskCard.css";
-// import { TaskItem } from './types';
+// import { TaskItem } from "./types";
+
 interface TaskProp {
   todoTitle: string;
-  todoDescription:string;
-  todoDueDate:string;
-  onDelete:()=>void;
+  todoDescription: string;
+  todoDueDate: string;
+  onDelete: () => void;
 }
 
-const Task=(props:TaskProp)=>{
+const Task = (props: TaskProp) => {
   return (
-    <li className="TaskItem">
-      <div className="shadow-md border border-slate-100">
-        <h2 className="text-base font-bold my-1">Title:{props.todoTitle}</h2>
-        <p className=" text-sm text-slate-500">
-          Due Date:{props.todoDueDate}
-        </p>
-        <p className="text-sm text-slate-500">
-          Description:{props.todoDescription} 
-        </p>
-        <button
-          className="deleteTaskButton"
-            onClick={() => props.onDelete()}>
-            Delete
-        </button>
-      </div>
-      </li>
-    );
+    <div className="TaskItem shadow-md border border-slate-100">
+      <h2 className="text-base font-bold my-1">Title: {props.todoTitle}</h2>
+      <p className="text-sm text-slate-500">Due Date: {props.todoDueDate}</p>
+      <p className="text-sm text-slate-500">Description: {props.todoDescription}</p>
+      <button className="deleteTaskButton" onClick={props.onDelete}>
+        Delete
+      </button>
+    </div>
+  );
 };
-  
+
 export default Task;
