e5752db31ee908cfb30b97a29793f40b0f1e4e5c
saixrr <111263091+saixrr@users.noreply.github.com>
2023-09-01 23:27:33 +0530

    commit changes for leve5

---
+++ smarter-tasks/src/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }: { children: JSX.Element }) {
+  const authenticated = localStorage.getItem("authenticated");
+
+  if (authenticated === 'true') {
+    return <>{children}</>;
+  } else {
+    const currentPath = window.location.pathname;
+    if (currentPath !== "/notfound") {
+      return <Navigate to="/notfound" />;
+    } else {
+      return <>{children}</>;
+    }
+  }
+}
