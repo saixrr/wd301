4aabc448c941d7ddebac3a7ad30d59b8acb59124
saixrr <111263091+saixrr@users.noreply.github.com>
2023-09-01 23:24:10 +0530

    Update App.tsx

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,10 +1,58 @@
-import TaskApp from "./TaskApp";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TaskListPage from "./pages/TaskListPage";
+import TaskDetailsPage from "./pages/TaskDetailsPage";
+import Signin from "./pages/Signin";
+import ProtectedRoute from "./ProtectedRoute";
+import Layout from "./Layout";
+import NotFound from "./pages/NotFound";
 
-function App() {
-  return (
-    <div className="App">
-      <TaskApp />
-    </div>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/signin" replace />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "tasks",
+        element: <TaskListPage />,
+      },
+      {
+        path: "tasks/:id",
+        element: <TaskDetailsPage />,
+      },
+      {
+        path: "notfound",
+        element: <NotFound />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  }
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
 export default App;
