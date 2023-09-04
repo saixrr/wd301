b79e6086deab10da1d2e7582f1a235dcd6881f47
saixrr <111263091+saixrr@users.noreply.github.com>
2023-09-04 19:35:27 +0530

    Update ProtectedRoute.tsx

--- smarter-tasks/src/ProtectedRoute.tsx
+++ smarter-tasks/src/ProtectedRoute.tsx
@@ -1,11 +1,10 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const isAuthenticated = localStorage.getItem("authenticated") === "true";
-
-  if (isAuthenticated) {
+  const authenticated = !!localStorage.getItem("authToken");
+  if (authenticated) {
     return <>{children}</>;
   } else {
     return <Navigate to="/signin" />;
-  }
+ }
 }
