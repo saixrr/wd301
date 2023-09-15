61c6e6e42dc6081ea07d173614117ace59cb0ced
saixrr <111263091+saixrr@users.noreply.github.com>
2023-09-15 11:15:28 +0530

    Update App.tsx

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,48 +1,19 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Notfound from "./pages/Notfound";
-import Signup from './pages/signup';
-import Signin from './pages/signin';
-import Dashboard from "./pages/dashboard";
-import ProtectedRoute from "./ProtectedRoute";
+import { useContext } from "react";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import router from "./routes"
+import { ThemeContext } from "./context/theme";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Signup />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/signin", 
-    element: <Signin />,
-  },
-  {
-    path: "/notfound",
-    element: <Notfound />,
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "*",
-    element: <Notfound />,
-  }
-]);
+import { ProjectsProvider } from "./context/projects/context";
 
 const App = () => {
+  const { theme } = useContext(ThemeContext)
   return (
-    <RouterProvider router={router} />
+     <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
+      <ProjectsProvider>
+        <RouterProvider router={router} />
+      </ProjectsProvider>
+    </div>
   );
 }
-
-export default App
+export default App;
