14e1716825e9a44bc09d6cb028d61cf20c27dd71
saixrr <111263091+saixrr@users.noreply.github.com>
2023-09-16 16:03:56 +0530

    Update App.tsx

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -5,14 +5,17 @@ import router from "./routes"
 import { ThemeContext } from "./context/theme";
 
 import { ProjectsProvider } from "./context/projects/context";
+import { MembersProvider } from "./context/members/context";
 
 const App = () => {
   const { theme } = useContext(ThemeContext)
   return (
      <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
-      <ProjectsProvider>
-        <RouterProvider router={router} />
-      </ProjectsProvider>
+      <MembersProvider>
+        <ProjectsProvider>
+          <RouterProvider router={router} />
+        </ProjectsProvider>
+  </MembersProvider>
     </div>
   );
 }
