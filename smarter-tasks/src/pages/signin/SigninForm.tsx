14dda218762ce4a55d020fa2495ab6eee0e50d51
saixrr <111263091+saixrr@users.noreply.github.com>
2023-09-16 14:23:16 +0530

    Update SigninForm.tsx

--- smarter-tasks/src/pages/signin/SigninForm.tsx
+++ smarter-tasks/src/pages/signin/SigninForm.tsx
@@ -32,7 +32,7 @@ const SigninForm: React.FC = () => {
       localStorage.setItem('authToken', data.token);
       localStorage.setItem('userData', JSON.stringify(data.user))
 
-      navigate('/dashboard');
+      navigate('/account/projects');
 
     } catch (error) {
       console.error('Sign-in failed:', error);
