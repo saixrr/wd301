8b01070cbbaf9f5b978143affb158c5fb2a64fb2
saixrr <111263091+saixrr@users.noreply.github.com>
2023-09-16 16:00:28 +0530

    Update MemberListItems.tsx

--- smarter-tasks/src/pages/members/MemberListItems.tsx
+++ smarter-tasks/src/pages/members/MemberListItems.tsx
@@ -1,12 +1,16 @@
 // import React from 'react';
 import { useMembersState } from  "../../context/members/context";
+import { SubmitHandler } from "react-hook-form";
+import { useMembersDispatch } from "../../context/members/context";
+import { deleteMember } from "../../context/members/actions";
 
-// type Inputs = {
-//   id: number;
-// }
+type Inputs = {
+  id: number;
+}
 
 export default function MemberListItems() {
   const state: any = useMembersState();
+  const dispatchMembers=useMembersDispatch();
   if (!state) {
     return <span>Loading...</span>; // Or handle the loading state accordingly
   }
@@ -25,17 +29,31 @@ export default function MemberListItems() {
     return <span>{errorMessage}</span>;
   }
 
+  const onSubmit:SubmitHandler<Inputs>=async (Id)=>{
+    const {id} = Id;
+    const response = await deleteMember(dispatchMembers,{ id });
+    if(response.ok){
+      console.log("user deleted")
+    }else {
+      console.log("user not deleted")
+    }
+  }
+
   return (
     <>
-      {members.map((member:any) => (
+      {members.map((user:any) => (
         <div
-          key={member.id}
+          key={user.id}
           className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">
-            {member.name}
+            {user.name}
           </h5>
-          <p className="text-gray-600 dark:text-gray-300">{member.email}</p>
+          <p className="text-gray-600 dark:text-gray-300">{user.email}</p>
+          <button id ="delete-member-btn" type="submit" onClick={()=>onSubmit({id:user.id})} className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+          >
+            delete
+          </button>
         </div>
       ))}
     </>
