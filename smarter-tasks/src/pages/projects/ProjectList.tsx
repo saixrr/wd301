29dd2ef3dcbd47c4ed14723a5540158e91ebef07
saixrr <111263091+saixrr@users.noreply.github.com>
2023-09-24 23:34:56 +0530

    Update ProjectList.tsx

--- smarter-tasks/src/pages/projects/ProjectList.tsx
+++ smarter-tasks/src/pages/projects/ProjectList.tsx
@@ -1,16 +1,7 @@
-import { useEffect } from 'react';
-import { fetchProjects } from "../../context/projects/actions"; // Import fetchProjects
-
-import { useProjectsDispatch } from "../../context/projects/context";
-import ProjectListItems from './ProjectListItems';
+import React from "react";
+import ProjectListItems from "./ProjectListItems";
 
 const ProjectList: React.FC = () => {
-  const dispatchProjects = useProjectsDispatch();
-  
-  useEffect(() => {
-    fetchProjects(dispatchProjects); // Use fetchProjects directly
-  }, []);
-
   return (
     <div className="grid gap-4 grid-cols-4 mt-5">
       <ProjectListItems />
