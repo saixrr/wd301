8d5e3198369928c6b3804b39156fb04a0aecf79a
saixrr <111263091+saixrr@users.noreply.github.com>
2023-10-02 12:09:34 +0530

    Update index.tsx

--- smarter-tasks/src/pages/project_details/index.tsx
+++ smarter-tasks/src/pages/project_details/index.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-
-import ProjectDetails from "./ProjectDetails";
+import React,{Suspense} from "react";
+const ProjectDetails = React.lazy(() => import("./ProjectDetails"));
 import { TasksProvider } from "../../context/task/context";
-
+import ErrorBoundary from "../../components/ErrorBoundary";
 import { Outlet } from "react-router-dom";
 
 const ProjectDetailsIndex: React.FC = () => {
     return (
       <TasksProvider>
         <ProjectDetails />
-        <Outlet />
+        <ErrorBoundary>
+        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </ErrorBoundary>
       </TasksProvider>
     );
   };
