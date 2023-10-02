61268fb661c7db6f29d2ba69a600fbd436d9078d
saixrr <111263091+saixrr@users.noreply.github.com>
2023-10-02 12:09:01 +0530

    Update index.tsx

--- smarter-tasks/src/pages/members/index.tsx
+++ smarter-tasks/src/pages/members/index.tsx
@@ -1,6 +1,9 @@
 // import React from 'react';
-import MemberList from './MemberList';
+import React, { Suspense } from "react";
+
+const MemberList = React.lazy(() => import("./MemberList"));
 import NewMember from './NewMember';
+import ErrorBoundary from "../../components/ErrorBoundary";
 
 const Members = () => {
   return (
@@ -9,7 +12,11 @@ const Members = () => {
         <h2 className="text-2xl font-medium tracking-tight">Members</h2>
         <NewMember />
       </div>
-      <MemberList />
+      <ErrorBoundary>
+        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+          <MemberList />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
